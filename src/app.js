db6651953aa4aaff800e6a95598f437813060fb0
gisele <giselesilva2311@gmail.com>
2023-04-15 22:41:27 -0300

    participants post

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import { MongoClient } from "mongodb"
 import dotenv from "dotenv"
+import joi from "joi"
 
 const app = express()
 app.use(cors())
@@ -17,6 +18,65 @@ mongoClient.connect()
 .catch((err) => console.log(err.message))
 //Fim da Conexão do banco de dados
 
+app.post("/participants", async(req, res) => {
+    const { name } = req.body
+    
+    const userSchema = joi.object({
+        name: joi.string().required()
+    })
+
+    const validation = userSchema.validate(name, {abortEarly: false})
+
+    if(validation.error){
+        const errors = validation.error.details.map((detail) => detail.message)
+        return res.status(422).send(errors)
+    }
+    
+    try {
+        const userExist = await db.collection("participants").findOne({name: name})
+        if (userExist) return res.status(409).send("Usuário já cadastrado")
+
+        db.collection("participants").insertOne({
+            name,
+            lastStatus: Date.now()
+        })
+        res.sendStatus(201)
+    } catch (error) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.get("/participants", async(req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/messages", async(req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.get("/messages", async(req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).send(err.message)
+    }
+})
+
+app.post("/status", async(req, res) => {
+    try {
+        
+    } catch (error) {
+        res.status(500).send(err.message)
+    }
+})
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
