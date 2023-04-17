0e7b216b637fb1d35d4249f49931ad5674959a48
gisele <giselesilva2311@gmail.com>
2023-04-16 23:11:39 -0300

    setInterval

--- src/app.js
+++ src/app.js
@@ -112,7 +112,7 @@ app.get("/messages", async(req, res) => {
         
         const findMessages = messages.filter((message) => {
 
-            const messagePrivate = message.to==="todos" || message.to===user ||message.from===user
+            const messagePrivate = message.to==="Todos" || message.to===user ||message.from===user
             const messagePublic = message.type === "message"
             return messagePrivate || messagePublic
         })
@@ -144,7 +144,31 @@ app.put("/status", async(req, res) => {
     }
 })
 
+setInterval (async () => {
+    const seconds = Date.now() - 10000
+    
+    try {
+        const inactive = await db.collection("participants").find({lastStatus: {$lte: seconds}}).toArray()
 
+        if (inactive.length > 0) {
+            const lastMessage = inactive.map((inactive) => {
+                return {
+                    from: inactive.name,
+                    to: "Todos",
+                    text: 'sai da sala...',
+                    type: 'status',
+                    time: dayjs().format("HH:mm:ss")
+                }
+            })
+            await db.collection("messages").insertMany(lastMessage)
+            await db.collection("participants").deleteMany({lastStatus: {$lte: seconds}})
+        }
+
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+
+}, 15000)
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
